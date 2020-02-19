8e650db73d55afc30faf9423c0b2a75ac1e5ff22
ken <tad.kwon917@gmail.com>
2020-02-19 19:01:44 +0900

    Msg

---
+++ ng-welcome-msg-app/src/app/i18n-support.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class I18nSupportService {
+  private welcomeMsg;
+
+  constructor() { 
+    this.welcomeMsg = {
+      'ko' : '안녕하세요',
+      'en' : 'Hello',
+      'fr' : 'Bonjour'
+    };
+  }
+
+  getWelcomeMsgByCode(userName:string , code:string){
+    const helloMsg = this.welcomeMsg[code];
+    return `${helloMsg} , ${userName}`;
+  }
+
+}
