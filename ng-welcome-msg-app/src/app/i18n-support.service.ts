0c3bcf2bc14a4e9ca56045af1779ed5c61de30f8
Ken <tad.kwon917@gmail.com>
2020-03-16 01:11:42 +0900

    젠장

--- ng-welcome-msg-app/src/app/i18n-support.service.ts
+++ ng-welcome-msg-app/src/app/i18n-support.service.ts
@@ -1,21 +1,16 @@
 import { Injectable } from '@angular/core';
+import { LANG_METADATA } from './lang-metadata';
 
 @Injectable()
 export class I18nSupportService {
   langCode = 'ko';
-  private welcomeMsg;
 
   constructor() { 
-    this.welcomeMsg = {
-      'ko' : '안녕하세요',
-      'us' : 'Hello',
-      'fr' : 'Bonjour'
-    };
   }
 
-  getWelcomeMsgByCode(userName:string){
-    const helloMsg = this.welcomeMsg[this.langCode];
-    return `${helloMsg} , ${userName}!`;
+  getWelcomeMsg(userName:string){
+    const langData = LANG_METADATA.find(lang => lang.code === this.langCode);
+    return `${langData.msg} , ${userName}!`;
   }
 
 }
